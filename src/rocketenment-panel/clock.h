fa94a34613d677816fff64f630e7b3d7fa7de807
Taki <takismartgamer123@gmail.com>
2026-07-25 13:50:15 +0100

    Create clock.h

---
+++ src/rocketenment-panel/clock.h
@@ -0,0 +1,25 @@
+#ifndef ROCKETENMENT_CLOCK_H
+#define ROCKETENMENT_CLOCK_H
+
+#include <Elementary.h>
+
+
+typedef struct
+{
+    Evas_Object *label;
+    Ecore_Timer *timer;
+
+} RocketClock;
+
+
+RocketClock *
+rocket_clock_create(Evas_Object *parent);
+
+
+
+void
+rocket_clock_destroy(RocketClock *clock);
+
+
+
+#endif
